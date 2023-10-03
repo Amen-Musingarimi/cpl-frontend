5e2d004f7362a5785e8544f72a62c909d42dc576
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-10-03 15:07:40 +0200

    Add side bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { FiMenu } from 'react-icons/fi';
+import { Route, Routes } from 'react-router-dom';
+import SideBar from './components/sideBar/SideBar';
+import Authentication from './components/auth/Authentication';
+import { toggleNav } from './redux/sideBarSlice';
 import './App.css';
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <h2>CPL</h2>
+    <div className="main-container">
+      <FiMenu className="menu-icon" onClick={() => dispatch(toggleNav())} />
+
+      <SideBar />
+      <Routes>
+        <Route exact path="/auth" element={<Authentication />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
